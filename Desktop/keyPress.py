0e19eb674419ad2325f1f2af63d7ce7901bc91d4
colton <cloftus@princeton.edu>
2022-01-01 15:17:07 -0500

    cmd parsing

--- Desktop/keyPress.py
+++ Desktop/keyPress.py
@@ -3,6 +3,10 @@ from threading import Thread
 from numpy import result_type
 import pyautogui
 import enum
+from typing import List
+
+from torch.cuda import current_device
+
 
 class mode(enum.Enum):
     COMMAND = 1
@@ -10,7 +14,8 @@ class mode(enum.Enum):
     SHELL = 3
     SLEEP = 4
 
-modifiers = {'ctrl', 'alt', 'shift', 'super'}
+
+
 
 def handle_transcription(transcription, current_mode):
     # Only use the first index since we are only parsing one wav file
@@ -56,13 +61,32 @@ def _run_dictation(transcription):
 
 
 def _run_shell(transcription, safety_time=10):
+    
+    '''
+    it is dangerous to run shell commands based off voice input.
+    However, it is also very useful since the user can run any command
+    in their .basrc/config.fish/etc. This is very convenient
+
+    it is impossible to check every unsafe command in the shell since the 
+    user can alias commands and what exactly is not safe  depends on the user's
+    machine. The best way is to automatically create a nonblocking pop up, 
+    that if not interacted with, will automatically close and execute the command.  
+      
+    If you need low latency, just using the terminal may be preferred.
+
+    If you want convenience and safety, this may be better. 
+    '''
+
     import time
     import subprocess
     from multiprocessing import Process
     from os import environ
+
     shell = environ['SHELL']
     message = 'Are you sure you want to run command \'{}\' in current shell {}? \n \
-         Running command automatically in {} seconds'.format(transcription, shell, safety_time)
+         Unless canceled, the command will be run automatically {} seconds after \
+             this window first appeared'\
+         .format(transcription, shell, safety_time)
 
     q = multiprocessing.Queue()
 
@@ -73,13 +97,13 @@ def _run_shell(transcription, safety_time=10):
     time.sleep(safety_time)
 
     alert_thread.terminate()
+
     # check to see if the queue returned a message that would indicate a cancel
     try:
         result = q.get(block=False)
         #  we only need to check for cancel since that is the only thing it can return
         if result == 'CANCEL':
             print(f'Cancelled command: {transcription}')
-            return
     # An exception means the queue was empty and the user did not cancel
     except:
         subprocess.call([shell, '-i', '-c', transcription])
@@ -87,19 +111,74 @@ def _run_shell(transcription, safety_time=10):
 
 # This has to be wrapped in a process because alert is blocking
 def _alert_wrapper(message, q):
-    result = pyautogui.alert(text=message, title='Error', button='CANCEL')
+    result = pyautogui.alert(text=message, title='Safety Check Shell Command', button='CANCEL')
     q.put(result)
 
-def _run_command(transcription):
-    for key in modifiers:
-        if key in transcription:
-            pass
-    for word in transcription:
-        if word in alphabet:
-            pass    
+def _run_command(transcription, alphabet):
+    '''
+    Command Format:
+
+    (modifiers)* (alphabet)* || (focus/close/open) (editor/terminal/browser)
     
+    '''
+
+    modifiers = {'ctrl', 'alt', 'shift', 'super'}
+    window_actions = {'focus', 'open', 'close'}
+    applications = {'editor', 'terminal', 'browser'}
+    
+    cmdList: List[str] = []
+    currentCmd = []
+
+    currCmdHasAction = False
+
+    for word in transcription:
+
+
+        # modifiers only begin cmds or follow other modifiers
+        if word in modifiers:
+            lastTerm = currentCmd[-1]
+            if lastTerm not in modifiers:
+                cmdList.append(currentCmd)
+                currentCmd = [word]
+            if lastTerm in modifiers:
+                currentCmd.append(word)
+
+        # There can only be one cmd term for every cmd
+        # i.e. it doesn't make sense to have 'close focus browser'
+        if word in window_actions:
+            if currCmdHasAction:
+                cmdList.append(currentCmd)
+                currCmdHasAction = False
+                currentCmd = [word]
+
+            else:
+                currCmdHasAction = True    
+                currentCmd.append(word)
+
+        
+        elif word in alphabet:
+            currentCmd.append(alphabet[word])
+
+        elif word in applications:
+            currentCmd.append(word)
+
+
+class command_list:
+    current_command = []
+    command_list = []
+
+    def __init__(self):
+        return
+
+    def add_to_current_command(self, command: str):
+        self.current_command.append(command)  
+
+    def finish_this_command(self, command: str):
+        self.command_list.append(command)
+        self.current_command = []
+
 
 if __name__ == '__main__':
     # print(_run_dictation("command mode"))
     # print(_run_dictation("test command"))
-    _run_shell("echo test", safety_time=3)
+    # _run_shell("echo test", safety_time=10)Hello world!
